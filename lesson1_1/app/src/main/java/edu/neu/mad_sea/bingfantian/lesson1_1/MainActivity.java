428b8d667415d78487adfcaf3761bddb54e68350
B-Tian1996 <90421854+BingfanTian96@users.noreply.github.com>
2021-09-13 19:04:03 -0700

    Update lesson 1_2

--- lesson1_1/app/src/main/java/edu/neu/mad_sea/bingfantian/lesson1_1/MainActivity.java
+++ lesson1_1/app/src/main/java/edu/neu/mad_sea/bingfantian/lesson1_1/MainActivity.java
@@ -3,6 +3,7 @@ package edu.neu.mad_sea.bingfantian.lesson1_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("MainActivity", "MainActivity layout is complete");
     }
 }
